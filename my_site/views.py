827e256b719d18d439a50147ae184002684490b6
Denis Gatin <chizel@inbox.ru>
2014-02-14 13:58:31 +0200

    chapter 5

--- my_site/views.py
+++ my_site/views.py
@@ -1,12 +1,27 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.security import login_required, current_user
 from forms import LoginForm
-from my_site import app
+from my_site import app, lm, oid, db
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/')
 @app.route('/index')
 
+@login_required
 def index():
-    user = { 'nickname': 'Miguel' }
+    user = g.user
     posts = [
         { 
             'author': { 'nickname': 'John' }, 
@@ -17,7 +32,7 @@ def index():
             'body': 'The Avengers movie was so cool!' 
         }
     ]
-    return render_template("index.html",
+    return render_template('index.html',
         title = 'Home',
         user = user,
         posts = posts)
@@ -32,4 +47,25 @@ def login():
         title = 'Sign In',
         form = form,
         providers = app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email = resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+#need add checking is exist same name in db
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
 
