0795ecc564b7712c08621c9fe07ef63afb0b5c06
Denis Gatin <chizel@inbox.ru>
2014-02-26 19:02:35 +0200

    chapter 13 complete

--- my_site/__init__.py
+++ my_site/__init__.py
@@ -5,11 +5,12 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from flask.ext.mail import Mail
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
-
+from momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.debug = True
+app.jinja_env.globals['momentjs'] = momentjs
 
 db = SQLAlchemy(app)
 
